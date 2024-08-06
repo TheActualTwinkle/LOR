bf890aacd5b8b93e618b3ac29f8ae19644408371
Yaroslav Myakinin <y.myakinin@gmail.com>
2024-08-06 19:00:50 +0700

    Update: Repositories

--- Lor.DatabaseApp/Infrastructure/DatabaseApp.Persistence/Repositories/GroupRepository.cs
+++ Lor.DatabaseApp/Infrastructure/DatabaseApp.Persistence/Repositories/GroupRepository.cs
@@ -8,12 +8,12 @@ namespace DatabaseApp.Persistence.Repositories;
 public class GroupRepository(IDatabaseContext context)
     : RepositoryBase<Group>(context), IGroupRepository
 {
-    public Task<Group?> GetGroupByGroupName(string groupName, CancellationToken cancellationToken) =>
-        _context.Groups
+    public async Task<Group?> GetGroupByGroupName(string groupName, CancellationToken cancellationToken) =>
+        await _context.Groups
             .FirstOrDefaultAsync(u => u.GroupName == groupName, cancellationToken);
 
-    public Task<Group?> GetGroupByGroupId(int groupId, CancellationToken cancellationToken) => 
-        _context.Groups
+    public async Task<Group?> GetGroupByGroupId(int groupId, CancellationToken cancellationToken) => 
+        await _context.Groups
             .FirstOrDefaultAsync(u => u.Id == groupId, cancellationToken);
 
     public async Task<Dictionary<int, string>?> GetGroups(CancellationToken cancellationToken) => 
