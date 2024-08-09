3baa8f511a88cab21768d4103c3caad3ea1e0462
Yaroslav Myakinin <y.myakinin@gmail.com>
2024-08-09 22:36:30 +0700

    Add: New fields to proto file

---
+++ Lor.DatabaseApp/Core/DatabaseApp.Application/Common/ExtensionsMethods/MapFieldFromDictionaryConverter.cs
@@ -0,0 +1,18 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Collections;
+
+namespace DatabaseApp.Application.Common.ExtensionsMethods;
+
+public static class MapFieldFromDictionaryConverterAsync
+{
+    public static async Task<MapField<TKey, TValue>> FromDictionary<TKey, TValue>(this MapField<TKey, TValue> mapField, 
+        IDictionary<TKey, TValue> dictionary)  
+    {
+        foreach (var (key, value) in dictionary)
+        {
+            mapField[key] = value;
+        }
+
+        return await Task.FromResult(mapField);
+    } 
+}
