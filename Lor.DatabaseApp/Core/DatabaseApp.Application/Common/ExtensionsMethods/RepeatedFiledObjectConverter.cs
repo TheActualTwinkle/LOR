3d84e9c219e2d5b5ecc820759579c7089bc8f89f
Yaroslav Myakinin <y.myakinin@gmail.com>
2024-08-10 22:03:20 +0700

    Update: Extensions methods

---
+++ Lor.DatabaseApp/Core/DatabaseApp.Application/Common/ExtensionsMethods/RepeatedFiledObjectConverter.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseApp.AppCommunication.Grpc;
+using DatabaseApp.Domain.Dto;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+
+namespace DatabaseApp.Application.Common.ExtensionsMethods;
+
+public static class RepeatedFiledObjectConverter
+{
+    public static async Task<RepeatedField<T>> FromList<T, TSource>(this RepeatedField<T> repeatedField, 
+        List<TSource> sourceList, 
+        Func<TSource, T> mapper) 
+        where T : IMessage
+    {
+        // repeatedField.Clear();
+        foreach (var sourceItem in sourceList)
+        {
+            var targetItem = mapper(sourceItem);
+            repeatedField.Add(targetItem);
+        }
+
+        return await Task.FromResult(repeatedField);
+    }
+}
