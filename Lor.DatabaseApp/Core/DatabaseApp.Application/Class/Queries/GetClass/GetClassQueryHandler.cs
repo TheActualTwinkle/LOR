a7cdb7e48982264e311ef0ac9121ceccc6f0f0d0
Yaroslav Myakinin <y.myakinin@gmail.com>
2024-08-12 21:08:29 +0700

    Add: GetClass and GetOutdatedClasses queries

---
+++ Lor.DatabaseApp/Core/DatabaseApp.Application/Class/Queries/GetClass/GetClassQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseApp.Domain.Repositories;
+using FluentResults;
+using MapsterMapper;
+using MediatR;
+
+namespace DatabaseApp.Application.Class.Queries.GetClass;
+
+public class GetClassQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) 
+    : IRequestHandler<GetClassQuery, Result<ClassDto>>
+{
+    public async Task<Result<ClassDto>> Handle(GetClassQuery request, CancellationToken cancellationToken)
+    {
+        Domain.Models.Class? someClass = await unitOfWork.ClassRepository.GetClassById(request.ClassId, cancellationToken);
+
+        if (someClass is null) return Result.Fail("Такой пары не существует.");
+
+        return await Task.FromResult(mapper.From(someClass).AdaptToType<ClassDto>());
+    }
+}
---
+++ Lor.DatabaseApp/Core/DatabaseApp.Application/Class/Queries/GetOutdatedClasses/GetOutdatedClassesQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseApp.Domain.Repositories;
+using FluentResults;
+using MapsterMapper;
+using MediatR;
+
+namespace DatabaseApp.Application.Class.Queries.GetOutdatedClasses;
+
+public class GetOutdatedClassesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) 
+    : IRequestHandler<GetOutdatedClassesQuery, Result<List<ClassDto>>>
+{
+    public async Task<Result<List<ClassDto>>> Handle(GetOutdatedClassesQuery request, CancellationToken cancellationToken)
+    {
+        List<Domain.Models.Class>? outdatedClasses = await unitOfWork.ClassRepository.GetOutdatedClasses(cancellationToken);
+
+        if (outdatedClasses is null || outdatedClasses.Count == 0) return Result.Fail("Нет устаревших пар");
+
+        return await Task.FromResult(mapper.Map<List<ClassDto>>(outdatedClasses));
+    }
+}
