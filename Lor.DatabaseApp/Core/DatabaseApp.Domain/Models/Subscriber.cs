fcf69d1dec86642f3173c6f604565fb45613be83
Yaroslav Myakinin <y.myakinin@gmail.com>
2024-08-22 23:05:08 +0700

    Update: Entity configuretion

---
+++ Lor.DatabaseApp/Core/DatabaseApp.Domain/Models/Subscriber.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseApp.Domain.Models;
+
+public class Subscriber : IEntity
+{
+    public int Id { get; init; }
+    public long TelegramId { get; init; }
+
+    public User User { get; init; } = null!;
+}
---
+++ Lor.DatabaseApp/Infrastructure/DatabaseApp.Persistence/EntityTypeConfiguration/SubscriberConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using DatabaseApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DatabaseApp.Persistence.EntityTypeConfiguration;
+
+public class SubscriberConfiguration : IEntityTypeConfiguration<Subscriber>
+{
+    public void Configure(EntityTypeBuilder<Subscriber> builder)
+    {
+        builder.ToTable("SUBSCRIBERS");
+        
+        builder.HasKey(s => s.Id).HasName("Subscriber_pkey");
+
+        builder.HasIndex(s => s.TelegramId, "tg_id_check").IsUnique();
+
+        builder.Property(s => s.Id)
+            .UseIdentityAlwaysColumn()
+            .HasColumnName("id");
+        builder.Property(s => s.TelegramId)
+            .HasColumnName("telegram_id");
+
+        builder.HasOne(s => s.User).WithOne(u => u.Subscriber)
+            .HasPrincipalKey<User>(u => u.TelegramId)
+            .HasForeignKey<Subscriber>(s => s.TelegramId)
+            .HasConstraintName("Subscriber_user_id_fkey");
+    }
+}
