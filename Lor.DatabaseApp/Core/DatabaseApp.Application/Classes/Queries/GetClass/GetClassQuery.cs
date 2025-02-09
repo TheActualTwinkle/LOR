0febb68896a1e2c1f238cb74a5a097c48a6d97a0
TheActualTwinkle <super.irums2@gmail.com>
2025-02-10 04:41:24 +0700

    Mark GetClassQuery as Obsolete

--- Lor.DatabaseApp/Core/DatabaseApp.Application/Classes/Queries/GetClass/GetClassQuery.cs
+++ Lor.DatabaseApp/Core/DatabaseApp.Application/Classes/Queries/GetClass/GetClassQuery.cs
@@ -3,6 +3,8 @@ using MediatR;
 
 namespace DatabaseApp.Application.Class.Queries;
 
+// TODO: Useless after 1d463466fb1e228f0b72fb05ff4f6d81eb76c711 commit. Has to be removed!
+[Obsolete("Will be removed soon...")]
 public record GetClassQuery : IRequest<Result<ClassDto>>
 {
     public required string ClassName { get; init; }
